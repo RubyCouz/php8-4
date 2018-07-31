f335c45974e5356a5efd43e811a559ecd56f2f18
rubycouzcedric <cousin.ruby.cedric@gmail.com>
2018-07-31 14:47:06 +0200

    ajout de css

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
-<?php
 
-?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8" />
   <title>Exercice 4</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-  <p>
-    <?php
-    echo $_COOKIE['pseudo'];
-    echo $_COOKIE['password'];
-    
-    ?>
-  </p>
+  <div class="modal" id="modalInfo"tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Vos identifiants</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>
+            <?php
+            echo 'Votre pseudo : ' . $_COOKIE['login']; ?>
+          </p>
+          <p>
+            <?php
+            echo 'Votre mot de passe : ' . $_COOKIE['password'];
+            ?>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+          <button type="button" class="btn btn-primary">Fermer aussi</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="assets/js/jquery-3.3.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" charset="utf-8"></script>
+  <script src="assets/js/script.js" charset="utf-8"></script>
+
+
 </body>
 </html>
